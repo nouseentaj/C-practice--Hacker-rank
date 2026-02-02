8c94c3db0e8a2b861f39188095d259ce97ce8021
KURNOOL NOUSEEN TAJ <254g1a33b4@srit.ac.in>
2026-02-02 19:56:29 +0530

    Create challenge8.c

---
+++ challenge8.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    int n;
+    scanf("%d", &n);
+  	// Complete the code to print the pattern.
+    int size = 2*n-1;
+    
+    for(int i=0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            int top =i;
+            int left = j;
+            int right = size-1-j;
+            int bottom = size-1-i;
+            
+            int min = top;
+            if(left<min)min = left;
+            if(right<min)min = right;
+            if(bottom<min)min = bottom;
+            printf("%d ",n-min);
+        }
+        printf("\n");
+    }
+    return 0;
+}
